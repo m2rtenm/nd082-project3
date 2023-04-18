87a37a5fc8740ddf0c21c5572f8253bfbd587423
Märten Mändla <marten.mandla@gmail.com>
2023-04-18 23:48:36 +0300

    Error fix

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -10,7 +10,7 @@ resource "azurerm_linux_web_app" "Linux_WebApp" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  service_plan_id     = azurerm_service_plan.test.id
+  service_plan_id     = azurerm_service_plan.az_service_plan.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -7,6 +7,6 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "v_subnet" {
   name                 = "${var.application_type}-${var.resource_type}-sub"
   resource_group_name  = "${var.resource_group}"
-  virtual_network_name = "${azurerm_virtual_network.test.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
   address_prefixes = [ var.address_prefix_test]
 }
